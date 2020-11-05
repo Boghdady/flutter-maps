9293647567649fbd5e73cf886176a75d81bd0c30
boghdady <boghdady107@gmail.com>
2020-11-05 15:01:35 +0200

    Prepare login and register form with validation

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -6,12 +6,79 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _frmRegisterKey = GlobalKey<FormState>();
+  TextEditingController _emailController;
+  TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(child: Text("LoginPage")),
+        padding: EdgeInsets.all(20.0),
+        child: Center(
+          child: Form(
+            key: _frmRegisterKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Must enter email';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: 'Passwoed',
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Must enter email';
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                ),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                  onPressed: btnLogin,
+                  child: Text("Login"),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  void btnLogin() {
+    if (_frmRegisterKey.currentState.validate()) {
+      print(_emailController.text);
+      print(_passwordController.text);
+    }
   }
 }
--- lib/Pages/registeration_page.dart
+++ lib/Pages/registeration_page.dart
@@ -8,12 +8,79 @@ class RegisterationPage extends StatefulWidget {
 }
 
 class _RegisterationPageState extends State<RegisterationPage> {
+  final _frmRegisterKey = GlobalKey<FormState>();
+  TextEditingController _emailController;
+  TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(child: Text("Registration Page")),
+        padding: EdgeInsets.all(20.0),
+        child: Center(
+          child: Form(
+            key: _frmRegisterKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Must enter email';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: 'Passwoed',
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Must enter email';
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                ),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                  onPressed: btnRegister,
+                  child: Text("Register"),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  void btnRegister() {
+    if (_frmRegisterKey.currentState.validate()) {
+      print(_emailController.text);
+      print(_passwordController.text);
+    }
   }
 }
