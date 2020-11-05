34f9407c8da752c27649128f7c44656b8fde0e42
boghdady <boghdady107@gmail.com>
2020-11-05 13:32:43 +0200

    setup firebase, google map, geolocator, firebase auth and prepare ui screens

---
+++ lib/Pages/registeration_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RegisterationPage extends StatefulWidget {
+  RegisterationPage({Key key}) : super(key: key);
+
+  @override
+  _RegisterationPageState createState() => _RegisterationPageState();
+}
+
+class _RegisterationPageState extends State<RegisterationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(child: Text("Registration Page")),
+      ),
+    );
+  }
+}
